0027903803b0299f6db9b3db79eebac21d607b09
박한 <bagkan1211@gmail.com>
2025-07-29 01:23:02 +0900

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -9,97 +9,115 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           centerTitle: false,
-          title: Text(
+          title: const Text(
             "xx동3가",
             style: TextStyle(
               fontWeight: FontWeight.bold,
             ),
           ),
-          actions: [
-            Container(
-              width: 200,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Icon(Icons.search),
-                  Icon(Icons.menu),
-                  Icon(Icons.notification_add),
-                ],
-              ),
-            )
+          actions: const [
+            IconButton(
+              onPressed: null,
+              icon: Icon(Icons.search),
+            ),
+            IconButton(
+              onPressed: null,
+              icon: Icon(Icons.menu),
+            ),
+            IconButton(
+              onPressed: null,
+              icon: Icon(Icons.notification_add),
+            ),
           ],
         ),
-
         body: Container(
-          width: double.infinity, height: 170,
-          padding: EdgeInsets.all(20),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                width: 130, height: 130,
-                clipBehavior: Clip.hardEdge,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
+          width: double.infinity,
+          height: 195,
+          padding: const EdgeInsets.fromLTRB(15, 40, 15, 15),
+          child: SizedBox(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Flexible(
+                  flex: 2,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.asset(
+                      "assets/camera.png",
+                      fit: BoxFit.cover,
+                    ),
+                  ),
                 ),
-                child: Image.asset(
-                  "camera.png",
-                ),
-              ),
-              Container(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      width: 300,
-                      child: Column(
+
+                const SizedBox(width: 20),
+
+                Flexible(
+                  flex: 3,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
-                            "캐논 DSLR 100D (단렌즈, 충전기 10기가SD 포함)",
+                            "캐논 DSLR 100D (단렌즈, 충전기 16기가SD 포함)",
                             style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.w600
+                              fontWeight: FontWeight.bold,
+                              fontSize: 17,
                             ),
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
                           ),
+
+                          SizedBox(height: 10),
+
                           Text(
-                            "xx구 xx동 - 박한 10분전",
+                            "xx구 xx동 · 박한 · 10분 전",
                             style: TextStyle(
+                              fontSize: 14,
                               color: Colors.grey,
                             ),
                           ),
+
+                          SizedBox(height: 10),
+
                           Text(
-                            "210,000원",
+                            "1000원",
                             style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
                             ),
-                          )
+                          ),
                         ],
                       ),
-                    ),
-                        Container(
-                          width: 300,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              Icon(Icons.thumb_up),
-                              SizedBox(width: 4,),
-                              Text("4", style: TextStyle(fontWeight: FontWeight.bold),)
-                            ]
-                          )
-                        )
-                  ],
+
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Icon(Icons.thumb_up, size: 18, color: Colors.grey),
+                          SizedBox(width: 4),
+                          Text(
+                            "4",
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              )
-            ],
+              ],
+            ),
           ),
-        )
+        ),
       ),
     );
   }
